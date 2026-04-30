23b71bb3b783803671fd9a751eb4f3e4e60842cf
unknown <73320813+RegulatedDev@users.noreply.github.com>
2026-04-30 10:36:45 -0400

    hiii
    jhklhl

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-const logo=document.getElementById("logo")
+const logo=document.getElementById("navbarLogo")
+const numOfLogos=15
 
+const randomNumber=Math.floor(Math.random()*numOfLogos)+1
+
+logo.src = `assets/logos/${randomNumber}.png`
+
+console.log("hi")
