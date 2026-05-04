2405286029486dd8f74b93af40b516153a1a1336
unknown <73320813+RegulatedDev@users.noreply.github.com>
2026-05-04 10:31:24 -0400

    pagess

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 const logo=document.getElementById("navbarLogo")
-const numOfLogos=15
 
-const randomNumber=Math.floor(Math.random()*numOfLogos)+1
+const logos = [
+  "Columbus_LogoLockUpRev_Blue_RGB.png",
+  "Columbus_LogoLockUpRev_Green_RGB.png",
+  "Columbus_LogoLockUpRev_Orange_RGB.png",
+  "Columbus_LogoLockUpRev_Pink_RGB.png",
+  "Columbus_LogoLockUpRev_Red_RGB.png",
+  "Columbus_LogoLockUpRev_Teal_RGB.png",
+  "Columbus_LogoLockUpRev_Yellow_RGB.png",
+]
 
-logo.src = `assets/logos/${randomNumber}.png`
+let currentLogo=null
 
-console.log("hi")
+function randomizeLogo(){
+	let randomIndex=Math.floor(Math.random()*logos.length)
+	while (randomIndex==currentLogo){
+		randomIndex=Math.floor(Math.random()*logos.length)
+		if (randomIndex!=currentLogo){
+			break
+		}
+	}
+	currentLogo=randomIndex
+	logo.src="../assets/logos/"+logos[randomIndex]
+}
+randomizeLogo()
+setInterval(randomizeLogo,1500)
