db41036ec510d698d9c486dc0ae4e3ea2660e619
unknown <73320813+RegulatedDev@users.noreply.github.com>
2026-05-11 10:24:48 -0400

    bussss

---
+++ js/BUS.js
@@ -0,0 +1,23 @@
+const button=document.getElementById("xhr")
+const flights=document.getElementById("flights")
+
+const url="https://api.aviationstack.com/v1/"
+
+let xhr=new XMLHttpRequest()
+
+xhr.onload=function(){
+	if(xhr.status===200){
+		const response=JSON.parse(xhr.responseText)
+		response.data.forEach(flight=>{
+			const li=document.createElement("li")
+			li.textContent=`plane ${flight.flight.iata} from ${flight.departure.airport} is arriving at ${flight.arrival.scheduled}`
+			flights.appendChild(li)
+        })
+	}
+}
+
+button.onclick=function(){
+	xhr.open("GET",url+"flights?access_key=16a0de86ad27ed61ead3af37cd5e64cc&arr_iata=CMH",true)
+
+	xhr.send(null)
+}
